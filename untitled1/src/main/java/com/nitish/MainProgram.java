2cf9bc4210af860a64dbfeb430e6867462741d79
N-itish <manandharnara@gmail.com>
2023-10-01 17:25:26 +0545

    test2

--- untitled1/src/main/java/com/nitish/MainProgram.java
+++ untitled1/src/main/java/com/nitish/MainProgram.java
@@ -3,6 +3,6 @@ package com.nitish;
 public class MainProgram {
     public static void main(String...args){
         Calculate calculate = new Calculate();
-        System.out.println(calculate.add(12,745));
+        System.out.println(calculate.add(123,745));
     }
 }
